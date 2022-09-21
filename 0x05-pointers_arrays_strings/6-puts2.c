1c9c4ad70430703ed7f4ca81c88bc8e1483e39a4
Agha_Joseph <ogbobest245@gmail.com>
2022-09-20 17:49:19 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Entry point to our function
+ * @str: a pointer variable that takes string
+ * Return: 0. For Success
+ */
+
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0)'
+	{
+		if (counter % 2 == 0)
+			_putchar(*(str + counter));
+		counter++;
+	}
+	_putchar(10);
+}
